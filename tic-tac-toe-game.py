fa7900c5cfdc18789ac698adfb7146e996159961
priyank <priyankagarwal6378@gmail.com>
2023-07-09 14:08:27 +0530

    minimax algorithm implemented

---
+++ tic-tac-toe-game.py
@@ -0,0 +1,193 @@
+import math
+import copy
+import os
+
+
+################ INITIALIZATION ###################
+
+X = 100
+O = -100
+tie = 0
+
+player_dict = {
+    X : " X ",
+    O : " O ",
+    tie : " _ "
+}
+
+state = [
+    [0, 0, 0],
+    [0, 0, 0],
+    [0, 0, 0]
+]
+
+state_dict = {}
+for a in [X, O, tie]:
+    for b in [X, O, tie]:
+        for c in [X, O, tie]:
+            for d in [X, O, tie]:
+                for e in [X, O, tie]:
+                    for f in [X, O, tie]:
+                        for g in [X, O, tie]:
+                            for h in [X, O, tie]:
+                                for i in [X, O, tie]:
+                                    tmp_state = [
+                                        [a, b, c],
+                                        [d, e, f],
+                                        [g, h, i]
+                                    ]
+                                    state_dict[str(tmp_state)] = len(state_dict)
+
+minimax_values = []
+for i in range(len(state_dict)):
+    minimax_values.append(None)
+
+############################################################
+
+
+################# TERMINAL STATE CHECKER ######################
+def winner(state):
+    for i in range(3):
+        if state[i][0] == state[i][1] == state[i][2] and state[i][0] != 0:
+            return state[i][0]
+        if state[0][i] == state[1][i] == state[2][i] and state[0][i] != 0:
+            return state[0][i]
+    if state[0][0] == state[1][1] == state[2][2] and state[0][0] != 0:
+        return state[0][0]
+    if state[0][2] == state[1][1] == state[2][0] != 0:
+        return state[0][2]
+
+    return tie
+############################ END ##############################
+
+###################### AVAILABLE MOVES #######################
+def available_moves(state):
+    moves = []
+    for i in range(3):
+        for j in range(3):
+            if state[i][j]==0:
+                moves.append([i,j])
+    return moves
+
+
+################### MINIMAX ALGORITHM #####################
+def minimax(state, depth, player):
+    s = copy.deepcopy(state)
+
+    winning_player = winner(s)
+    if  winning_player != tie:
+        if winning_player == X : 
+            return winning_player - depth
+        else :
+            return winning_player + depth
+
+    if len(available_moves(s)) == 0 :
+        return 0
+
+    if player == X:
+        max_val = -math.inf
+        for move in available_moves(s):
+            i = move[0]
+            j = move[1]
+            s[i][j] = player
+            val = minimax(s, depth+1, O)
+            s[i][j] = 0
+            max_val = max(max_val , val)
+        minimax_values[state_dict[str(s)]] = max_val
+
+        return max_val
+    
+    if player == O:
+        min_val = math.inf
+        for move in available_moves(s):
+            i = move[0]
+            j = move[1]
+            s[i][j] = player
+            val = minimax(s, depth+1, X)
+            s[i][j] = 0
+            min_val = min(min_val, val)
+        minimax_values[state_dict[str(s)]] = min_val
+
+        return min_val
+    
+#############################################################
+
+######################### MAX TURN ###########################
+def max_turn(state):
+    max_val = -math.inf
+    max_move = None
+    s = copy.deepcopy(state)
+    for move in available_moves(s):
+        i, j = move[0], move[1]
+        s[i][j] = X
+        val = minimax(s, 1, O)
+        if max_val < val:
+            max_val = val
+            max_move = move
+        s[i][j] = 0
+    return max_move
+
+###############################################################
+    
+######################### MIN TURN ###########################
+def min_turn(state):
+    min_val = math.inf
+    min_move = None
+    s = copy.deepcopy(state)
+    for move in available_moves(s):
+        i, j = move[0], move[1]
+        s[i][j] = O
+        val = minimax(s, 1, X)
+        if min_val > val:
+            min_val = val
+            min_move = move
+        s[i][j] = 0
+    return min_move
+
+###############################################################
+
+########################## PLAY ###############################
+def play(begin_state, begin_player):
+    s = begin_state
+    player = begin_player
+    os.system("clear")
+    while winner(s) == 0:
+        
+        if player == X:         #user
+            while True:
+                print("YOUR TURN!")
+                i,j = input("Enter your move: ").split()
+                i,j = int(i), int(j)
+                if s[i][j] != 0:
+                    print("Enter the move for empty square! Try again.")
+                else :
+                    break
+            s[i][j] = X
+            player = O
+        else :                  #AI
+            os.system("clear")
+            # print("MY TURN!")
+            move = min_turn(s)
+            i,j = move[0], move[1]
+            s[i][j] = O
+            player = X
+        
+        print('-'*11)
+        for i in range(3):
+            print("|" , end='')
+            for j in range(3):
+                print(player_dict[s[i][j]], end='')
+            print("|")
+            print('-'*11)
+        print()
+        print()
+
+########################################################
+
+
+
+
+########################### MAIN #############################
+if __name__ == "__main__":
+    print("You are the first player!")
+    play(state, X)
